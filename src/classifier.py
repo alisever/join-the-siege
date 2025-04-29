6af3d57cd7b5e4d5675e077c44ae07524a7c13aa
Ali Sever <alisever96@hotmail.com>
2025-04-29 08:58:43 +0100

    Added fuzzy matching to classify_file.

--- src/classifier.py
+++ src/classifier.py
@@ -1,17 +1,38 @@
+import logging
+import os
+
 from werkzeug.datastructures import FileStorage
+from rapidfuzz import fuzz
 
 
-def classify_file(file: FileStorage):
+logger = logging.getLogger(__name__)
+
+MINIMUM_CONFIDENCE = 70
+
+FILE_CLASSES = [
+    "drivers_licence",
+    "bank_statement",
+    "invoice",
+]
+
+
+def classify_file(file: FileStorage) -> str:
     filename = file.filename.lower()
+    filename = os.path.splitext(filename)[0]
     # file_bytes = file.read()
 
-    if "drivers_license" in filename:
-        return "drivers_licence"
+    best_match, best_score = "unknown_file", 0
 
-    if "bank_statement" in filename:
-        return "bank_statement"
+    for classification in FILE_CLASSES:
+        score = fuzz.partial_ratio(classification, filename)
+        if score > best_score:
+            best_match, best_score = classification, score
 
-    if "invoice" in filename:
-        return "invoice"
+    if best_score < MINIMUM_CONFIDENCE:
+        logger.warning(
+            "Low confidence classification for %s: %s with score %s"
+            % (file.filename, best_match, best_score)
+        )
+        return "unknown_file"
 
-    return "unknown file"
+    return best_match
---
+++ tests/test_classifier.py
@@ -0,0 +1,37 @@
+import os
+from io import BytesIO
+
+from werkzeug.datastructures import FileStorage
+import pytest
+from src.classifier import classify_file
+
+FILES_FOLDER = os.path.join(os.path.dirname(__file__), "../files")
+
+
+@pytest.mark.parametrize(
+    "file_name, expected_class",
+    [
+        ("bank_statement_1.pdf", "bank_statement"),
+        ("bank_statement_2.pdf", "bank_statement"),
+        ("bank_statement_3.pdf", "bank_statement"),
+        ("drivers_licence_2.jpg", "drivers_licence"),
+        ("drivers_license_1.jpg", "drivers_licence"),
+        ("drivers_license_3.jpg", "drivers_licence"),
+        ("invoice_1.pdf", "invoice"),
+        ("invoice_2.pdf", "invoice"),
+        ("invoice_3.pdf", "invoice"),
+        ("unknown_file_1.txt", "unknown_file"),
+        ("unknown_file_2.docx", "unknown_file"),
+        ("unknown_file_3.pptx", "unknown_file"),
+        ("unknown_file_4.jpg", "unknown_file"),
+        ("unknown_file_5.png", "unknown_file"),
+    ],
+)
+def test_classify_file(file_name, expected_class):
+    dummy_file = FileStorage(
+        stream=BytesIO(b"Dummy data"),
+        filename=file_name,
+        content_type="application/octet-stream",
+    )
+    result = classify_file(dummy_file)
+    assert result == expected_class
